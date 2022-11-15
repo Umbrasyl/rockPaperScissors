dbebab5c20b0174c7a2f1f83767ce1ba46cd5176
Murat ULUTÜRK <ctf.murat.uluturk@gmail.com>
2022-11-15 08:42:25 +0300

    Finish functionality. Add basic style.

--- script.js
+++ script.js
@@ -1,45 +1,68 @@
 "use strict";
 
+function playRound() {
+    const movesMade = makeMoves(this.dataset.move);
+    const result = movesMade[1] - movesMade[0];
+    return checkRoundEnd(result, movesMade[0], movesMade[1]);
+}
+
+function makeMoves(inputMove) {
+    const computerChoice =  generateCompChoice();
+    return [inputMove, computerChoice];
+}
+
 function generateCompChoice() {
     return Math.floor(Math.random() * 3);
 }
 
-function playRound() {
-    const movesMade = makeMoves();
-    const result = movesMade[1] - movesMade[0];
-    console.log(result);
-    const movesString = `You chose: ${movesArray[movesMade[0]]}, Computer chose: ${movesArray[movesMade[1]]}`;
+function checkRoundEnd(result, playerMove, compMove) {
+    resultText.textContent = "";
+    const movesString = `You chose: ${MOVESARRAY[playerMove]}, Computer chose: ${MOVESARRAY[compMove]}`;
     if (result === 0) {
-        console.log("It's a tie. " + movesString);
-        return 0;
+        resultText.textContent = "It's a tie. " + movesString;
     }else if (result === 2 || result === -1) {
-        console.log("You Win. " + movesString);
-        return 1;
+        resultText.textContent = "You Win. " + movesString;
+        keepScores(1);
     }else {
-        console.log("You Lose. " + movesString);
-        return -1;
+        resultText.textContent = "You Lose. " + movesString;
+        keepScores(-1);
     }
 }
 
-function makeMoves() {
-    const computerChoice =  generateCompChoice();
-    const playerChoice = movesArray.indexOf(prompt("Choose rock, paper or scissors: "));
-    return [playerChoice, computerChoice];
+function keepScores(roundResult) {
+    if (roundResult === 1) {
+        playerScore.textContent = Number(playerScore.textContent) + 1;
+        checkWin(+playerScore.textContent, "player");
+    } else if (roundResult === -1) {
+        compScore.textContent = Number(compScore.textContent) + 1;
+        checkWin(+compScore.textContent, "computer");
+    }
 }
 
-function game() {
-    let playerScore = 0;
-    let compScore = 0;
-    for(let i = 0; i < 1; i++) {
-        const roundResult = playRound();
-        if (roundResult > 0) {
-            playerScore++;
-        } else if (roundResult < 0) {
-            compScore++;
+function checkWin(score, whoWon) {
+    if (score === 5) {
+        if (whoWon === "player") {
+            gameEnd.textContent = "Congratulations! You Won!"
+        } else {
+            gameEnd.textContent = "Oh no! You Lost!"
         }
+        buttonArray.forEach((button) => {
+            button.removeEventListener("click", playRound);
+        });
     }
-    console.log(`Game Over! Final Score:\nYou: ${playerScore}\nComputer: ${compScore}`);
 }
 
-let movesArray = ["rock", "paper", "scissors"];
-game();
+const MOVESARRAY = ["rock", "paper", "scissors"];
+const buttonArray = [
+    document.querySelector("button[data-move='0']"),
+    document.querySelector("button[data-move='1']"),
+    document.querySelector("button[data-move='2']"),
+];
+const resultText = document.querySelector(".result");
+const playerScore = document.querySelector("strong[data-person=player]");
+const compScore = document.querySelector("strong[data-person=computer]");
+const gameEnd = document.querySelector(".game-end");
+
+buttonArray.forEach((button) => {
+    button.addEventListener("click", playRound);
+});
